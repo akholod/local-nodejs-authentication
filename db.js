7ee5a9fae5f30cc29e03e7a631e5b05bbc5d965a
akholod <andrey.kokhanevich@gmail.com>
2016-11-05 01:48:50 +0200

    rm db config

--- db.js
+++ db.js
@@ -2,7 +2,6 @@
  * Created by andrey on 31.10.16.
  */
 const mongoose = require('mongoose');
-const configDB = require('./config/db');
 mongoose.connect(process.env.MONGOLAB_URI);
 
 //create db connection
