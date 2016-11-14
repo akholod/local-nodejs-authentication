6253abd72740d746bbaa550f8cca8cb80e627412
akholod <andrey.kokhanevich@gmail.com>
2016-11-14 18:27:13 +0200

    add facebook auth

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
  * Created by andrey on 31.10.16.
  */
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://akholod:3v1a6l0e0r2a@ds011785.mlab.com:11785/fcc');
+mongoose.connect(process.env.MONGOLAB_URI);
 
 //create db connection
 var db = mongoose.connection;
